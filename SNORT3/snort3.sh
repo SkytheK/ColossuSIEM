6e4aeacd80c2acfee9c67af0a922aec382b22496
SkytheK <74800494+SkytheK@users.noreply.github.com>
2021-05-17 14:29:36 +0200

    Update snort3.sh

--- SNORT3/snort3.sh
+++ SNORT3/snort3.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#Update & Upgrade our environment
+sudo apt-get update
+sudo apt-get upgrade -y
+
+echo "####SNORT 3.O SERVICE"
+#Install all the necesary packages 
+sudo apt install -y build-essential libpcap-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev libunwind-dev
+
+#Crate tha enviroment
+mkdir snort-source-files
+cd snort-source-files
